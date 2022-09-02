544468b5181005636d97fe8361e8d226883bc0e8
Uttarayan Mondal <email@uttarayan.me>
2022-09-02 15:29:37 +0530

    [feat] add agprefs to json

---
+++ agprefstojson/src/main.rs
@@ -0,0 +1,29 @@
+use clap::Parser;
+use std::io::Read;
+
+#[derive(Parser, Debug)]
+#[clap(author, version, about, long_about = None)]
+pub struct Args {
+    #[clap(short, long, value_parser)]
+    pub output: Option<String>,
+    #[clap(short, long, value_parser)]
+    pub input: Option<String>,
+}
+
+fn main() -> anyhow::Result<()> {
+    let args = Args::parse();
+    let mut input = String::new();
+    if let Some(ref input_path) = args.input {
+        input = std::fs::read_to_string(input_path)?;
+    } else {
+        std::io::stdin().read_to_string(&mut input)?;
+    }
+    let agprefs = agprefs::Agpref::from_str(&input)?;
+    let ajson = serde_json::to_string_pretty(&agprefs)?;
+    if let Some(ref output) = args.output {
+        std::fs::write(output, ajson)?;
+    } else {
+        println!("{}", ajson);
+    }
+    Ok(())
+}
